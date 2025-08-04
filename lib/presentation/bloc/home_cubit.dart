bc79aef29187fad64d7fa4d33e34f6e79c3938f0
dwiki <dwiki@azuba.tech>
2025-08-04 08:42:49 +0700

    test - bloc cubit starter

--- lib/presentation/bloc/home_cubit.dart
+++ lib/presentation/bloc/home_cubit.dart
@@ -1,6 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:testone/domain/model/category.dart';
-import 'package:testone/domain/model/food.dart';
 import 'package:testone/domain/usecase/get_categories.dart';
 import 'package:testone/domain/usecase/get_foods.dart';
 
@@ -17,41 +15,4 @@ class HomeCubit extends Cubit<HomeState> {
        _getCategoriesUC = getCategoriesUC,
        super(HomeState.initial());
 
-  List<Category> _categories = [];
-
-  Future<void> loadInitialData() async {
-    emit(state.copyWith(isLoading: true));
-    final List<Food> foods = await _getFoodsUC();
-    final List<Category> categories = await _getCategoriesUC();
-    _categories = categories;
-    emit(
-      state.copyWith(foods: foods, categories: categories, isLoading: false),
-    );
-  }
-
-  Future<void> setSelectedCategory(Category category) async {
-    emit(state.copyWith(isLoading: true));
-    final foods = await _getFoodsUC.call(categoryId: category.id);
-    emit(
-      state.copyWith(
-        foods: foods,
-        categories: _categories,
-        selectedCategory: category,
-        isLoading: false,
-      ),
-    );
-  }
-
-  Future<void> resetSelectedCategory() async {
-    emit(state.copyWith(isLoading: true));
-    final foods = await _getFoodsUC();
-    emit(
-      state.copyWith(
-        foods: foods,
-        categories: _categories,
-        selectedCategory: null,
-        isLoading: false,
-      ),
-    );
-  }
 }
--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:testone/presentation/food_item_card.dart';
-
-import 'bloc/home_cubit.dart';
-import 'bloc/home_state.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,81 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Foods'), centerTitle: false),
-      body: BlocConsumer<HomeCubit, HomeState>(
-        listener: (context, state) {
-          if (state.errorMessage != null) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.errorMessage!),
-                backgroundColor: Colors.red,
-              ),
-            );
-          }
-        },
-        builder: (context, state) {
-          return _buildBody(state);
-        },
-      ),
-    );
-  }
-
-  Widget _buildBody(HomeState state) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-          child: Row(
-            children: state.categories.map((category) {
-              final isSelected = state.selectedCategory == category;
-              return Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: ChoiceChip(
-                  label: Text(category.name),
-                  selected: isSelected,
-                  showCheckmark: false,
-                  onSelected: (selected) {
-                    if (selected) {
-                      context.read<HomeCubit>().setSelectedCategory(category);
-                    } else {
-                      context.read<HomeCubit>().resetSelectedCategory();
-                    }
-                  },
-                  selectedColor: Theme.of(context).primaryColor,
-                  labelStyle: TextStyle(
-                    color: isSelected ? Colors.white : Colors.black,
-                  ),
-                ),
-              );
-            }).toList(),
-          ),
-        ),
-        Expanded(
-          child: GridView.builder(
-            padding: const EdgeInsets.all(8.0),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-              childAspectRatio: 0.75,
-            ),
-            itemCount: state.foods.length,
-            itemBuilder: (context, index) {
-              final food = state.foods[index];
-              return FoodItemCard(food: food);
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Future.microtask(
-      () => {if (mounted) context.read<HomeCubit>().loadInitialData()},
+      body: Container(),
     );
   }
 }
